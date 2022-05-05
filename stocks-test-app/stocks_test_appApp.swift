fbfbffd443916a10a24b887c9e5439d3449320bc
vladimir.kuzomenskyi <kuzomenskyi@gmail.com>
2022-05-05 13:18:58 +0300

    * Project's files were added; * .gitignore has been modified; * Cocoa pods initialized;

---
+++ stocks-test-app/stocks_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  stocks_test_appApp.swift
+//  stocks-test-app
+//
+//  Created by vladimir.kuzomenskyi on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct stocks_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
