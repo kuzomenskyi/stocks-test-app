f87005bc78c6f83cbbbf76292abca910c7c93892
vladimir.kuzomenskyi <kuzomenskyi@gmail.com>
2022-05-05 13:43:01 +0300

    * Project's structure has been created; * Placeholder code has been removed;

---
+++ stocks-test-app/Application/stocks_test_appApp.swift
@@ -0,0 +1,35 @@
+//
+//  stocks_test_appApp.swift
+//  stocks-test-app
+//
+//  Created by vladimir.kuzomenskyi on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct stocks_test_appApp: App {
+    // MARK: Constant
+    
+    // MARK: Private Constant
+    
+    // MARK: Variable
+    var body: some Scene {
+        WindowGroup {
+            #warning("Insert view here")
+            Spacer()
+        }
+    }
+    
+    // MARK: Private Variable
+    
+    // MARK: Init
+    
+    // MARK: Action
+    
+    // MARK: Private Action
+    
+    // MARK: Function
+    
+    // MARK: Private Function
+}
